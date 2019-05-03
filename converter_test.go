466c739d55ed3aed32db5f8086f41ceec70fdfd4
Arianxx <34032486+Arianxx@users.noreply.github.com>
2019-05-03 21:16:30 +0800

    feat: basic converter test

--- converter_test.go
+++ converter_test.go
@@ -36,3 +36,97 @@ func TestSimpleString_Parse_successful(t *testing.T) {
 		t.Fatal("expected", expectedCmd, "got", cmd)
 	}
 }
+
+func TestInteger_Parse(t *testing.T) {
+	raw := []byte(":1000\n\r")
+	s := NewInteger()
+	cmd, err, surplus := s.Parse(raw)
+	if len(surplus) != 0 {
+		t.Fatal("expected 0, got ", string(surplus))
+	}
+	if err != nil {
+		t.Fatal("expected error, got ", err)
+	}
+	if string(cmd.Args[0]) != "1000" {
+		t.Fatal("expected", "1000", "got", string(cmd.Args[0]))
+	}
+}
+
+func TestInteger_Parse2(t *testing.T) {
+	raw := []byte(":100")
+	s := NewInteger()
+	_, _, _ = s.Parse(raw)
+	raw = []byte("0\n\r")
+	cmd, err, surplus := s.Parse(raw)
+	if len(surplus) != 0 {
+		t.Fatal("expected 0, got ", string(surplus))
+	}
+	if err != nil {
+		t.Fatal("expected error, got ", err)
+	}
+	if string(cmd.Args[0]) != "1000" {
+		t.Fatal("expected", "1000", "got", string(cmd.Args[0]))
+	}
+}
+
+func TestBulkString_Parse(t *testing.T) {
+	raw := []byte("$6\n\rtesthh\n\r")
+	s := NewBulkString()
+	cmd, err, surplus := s.Parse(raw)
+	if len(surplus) != 0 {
+		t.Fatal("expected 0, got ", string(surplus))
+	}
+	if err != nil {
+		t.Fatal("expected error, got ", err)
+	}
+	if string(cmd.Args[0]) != "testhh" {
+		t.Fatal("expected", "testhh", "got", string(cmd.Args[0]))
+	}
+}
+
+func TestBulkString_Parse2(t *testing.T) {
+	raw := []byte("$6\n\rtest")
+	s := NewBulkString()
+	_, _, _ = s.Parse(raw)
+	raw = []byte("hh\n\r")
+	cmd, err, surplus := s.Parse(raw)
+	if len(surplus) != 0 {
+		t.Fatal("expected 0, got ", string(surplus))
+	}
+	if err != nil {
+		t.Fatal("expected error, got ", err)
+	}
+	if string(cmd.Args[0]) != "testhh" {
+		t.Fatal("expected", "testhh", "got", string(cmd.Args[0]))
+	}
+}
+
+func TestArray_Parse(t *testing.T) {
+	raw := []byte("*-1\n\r")
+	s := NewArray()
+	cmd, err, _ := s.Parse(raw)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+	if cmd != RespNil {
+		t.Fatal("expected", RespNil, "got", cmd)
+	}
+}
+
+func TestArray_Parse2(t *testing.T) {
+	raw := []byte("*2\n\r+test\n\r:1000\n\r\n\r")
+	s := NewArray()
+	cmd, err, surplus := s.Parse(raw)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+	if len(surplus) != 0 {
+		t.Fatal("expected", 0, "got", string(surplus))
+	}
+	if string(cmd.Args[0]) != "test" {
+		t.Fatal("expected", "test", "got", string(cmd.Args[0]))
+	}
+	if string(cmd.Args[1]) != "1000" {
+		t.Fatal("expected", "test", "got", string(cmd.Args[0]))
+	}
+}
